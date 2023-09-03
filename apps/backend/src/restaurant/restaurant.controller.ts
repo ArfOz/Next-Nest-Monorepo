0952758d35a85f5ccd3d0331d36effb96b4c1600
Arif Trends <arfoz1245@gmail.com>
2023-09-03 23:17:26 +0200

    add get all restaurant

--- apps/backend/src/restaurant/restaurant.controller.ts
+++ apps/backend/src/restaurant/restaurant.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-
 import { RestaurantService } from './restaurant.service';
 
 @Controller('restaurant')
 export class RestaurantController {
   constructor(private readonly restaurantService: RestaurantService) {}
 
-  @Get('getdata')
+  @Get('getrestaurant')
   getData() {
     return this.restaurantService.getData();
+  }
+  @Get('getall')
+  getAll() {
+    return this.restaurantService.getAll();
   }
 }
