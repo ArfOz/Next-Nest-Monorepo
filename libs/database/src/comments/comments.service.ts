6888baf6d52373d653522214d874dfcae80905dd
Arif Trends <arfoz1245@gmail.com>
2023-10-10 22:41:51 +0200

    add prisma comments module

---
+++ libs/database/src/comments/comments.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class CommentsDBService {
+    constructor(private prisma: PrismaService) {}
+    // async findUnique(where: Prisma.CommentssWhereUniqueInput) {
+    //     const data = await this.prisma.Commentss.findUnique({ where });
+    //     return data;
+    // }
+
+    // async getAll() {
+    //     const data = await this.prisma.Commentss.findMany();
+    //     return data;
+    // }
+
+    // async addComments(data: Prisma.CommentssCreateInput) {
+    //     const response = await this.prisma.Commentss.create({ data });
+    //     return response;
+    // }
+}
--- libs/database/src/database.module.ts
+++ libs/database/src/database.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { RestaurantDBModule } from './restaurant/restaurant.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  providers: [PrismaModule],
-  exports: [PrismaModule],
-  imports: [PrismaModule, RestaurantDBModule],
+    providers: [PrismaModule],
+    exports: [PrismaModule],
+    imports: [PrismaModule, RestaurantDBModule, CommentsModule],
 })
 export class DatabaseModule {}
