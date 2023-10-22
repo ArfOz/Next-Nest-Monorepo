efb9020f80f23ae4c3779ab9dd51e22ee10c60a7
Arif Trends <arfoz1245@gmail.com>
2023-10-22 23:02:48 +0200

    add user login

--- apps/backend/src/user/user.controller.ts
+++ apps/backend/src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { AllowUnauthorizedRequest } from '@exceptions';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserSignInJson } from './dtos';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @AllowUnauthorizedRequest()
+    @Post('signin')
+    async getAll(@Body() data: UserSignInJson) {
+        return await this.userService.signin(data.username, data.password);
+    }
+}
--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -1,4 +1,11 @@
+import { AuthService } from '@auth';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly authService: AuthService) {}
+    async signin(username, password) {
+        const data = await this.authService.signIn(username, password);
+        return data;
+    }
+}
