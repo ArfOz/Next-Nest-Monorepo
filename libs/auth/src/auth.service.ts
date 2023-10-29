3fe04ff8943c845aecc4a8e4b057b6c74ea505df
Arif Trends <arfoz1245@gmail.com>
2023-10-29 22:17:34 +0100

    add token expire time

--- libs/auth/src/auth.service.ts
+++ libs/auth/src/auth.service.ts
@@ -1,27 +1,22 @@
+import { ConfigType } from '@nestjs/config';
 import { UsersDBService } from '@database';
 import { UnauthorizedException, UnauthorizedExceptionType } from '@exceptions';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
+import generalConfig from '@config/src/general.config';
 
 @Injectable()
 export class AuthService {
     constructor(
+        @Inject(generalConfig.KEY)
+        private readonly generalCfg: ConfigType<typeof generalConfig>,
         private usersService: UsersDBService,
         private jwtService: JwtService
     ) {}
 
-    async SignIn(email: string, pass: string) {
-        const user = await this.usersService.findOne({ email });
-        if (!user) {
-            throw new UnauthorizedException(
-                UnauthorizedExceptionType.USER_NOT_REGISTERED,
-                new Error('THERE IS NO USER.'),
-                404
-            );
-        }
+    async SignIn(user: any, pass: string) {
         const isMatch = await bcrypt.compare(pass, user?.password);
-
         if (!isMatch) {
             throw new UnauthorizedException(
                 UnauthorizedExceptionType.WRONG_PASSWORD,
@@ -29,14 +24,16 @@ export class AuthService {
                 404
             );
         }
-
         const payload = {
             sub: user.id,
             username: user.username,
             email: user.email
         };
+
         return {
-            access_token: await this.jwtService.signAsync(payload)
+            access_token: await this.jwtService.signAsync(payload, {
+                expiresIn: `${this.generalCfg.jwt_expired!}d`
+            })
         };
     }
 }
