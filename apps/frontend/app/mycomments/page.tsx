604d00cb37edddfea363d81b9d3f54dd53eb3471
Arif Trends <arfoz1245@gmail.com>
2023-11-27 23:00:01 +0100

    fix take comments

--- apps/frontend/app/mycomments/page.tsx
+++ apps/frontend/app/mycomments/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
-const page = () => {
+const MyComments = () => {
+    const { data: session, status, update } = useSession();
+    const [comments, setComments] = useState({});
+
+    useEffect(() => {
+        const data = async () => {
+            const res = await fetch(
+                'http://localhost:3300/api/comments/mycomments',
+                {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${session?.accessToken}`
+                    }
+                }
+            );
+            const response = await res.json();
+            console.log('response', response);
+            setComments(response);
+        };
+        data();
+    }, []);
+
     return <div>page</div>;
 };
 
-export default page;
+export default MyComments;
