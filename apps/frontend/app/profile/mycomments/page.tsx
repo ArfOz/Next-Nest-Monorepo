467d3990ae84463d4ed29bb27c871a6c93f0e86c
Arif Trends <arfoz1245@gmail.com>
2023-12-03 22:48:13 +0100

    update my comments page

--- apps/frontend/app/profile/mycomments/page.tsx
+++ apps/frontend/app/profile/mycomments/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
+import { Comments } from '../../components/Comments';
+import { CommentDetails } from '../../Dtos';
 
 const MyComments = () => {
     const { data: session, status, update } = useSession();
-    const [comments, setComments] = useState({});
+    const [comments, setComments] = useState([]);
 
     useEffect(() => {
         const data = async () => {
@@ -19,13 +21,25 @@ const MyComments = () => {
                 }
             );
             const response = await res.json();
-            console.log('response', response);
-            setComments(response);
+            console.log('response', response.Data);
+            setComments(response.Data);
         };
         data();
     }, []);
 
-    return <div>page</div>;
+    const CommentsMapper = (
+        <div className="flex flex-row flex-wrap gap-4 p-8 content-start">
+            {comments.map((commentData: CommentDetails) => (
+                <Comments key={commentData.id} comments={commentData} />
+            ))}
+        </div>
+    );
+
+    const NoComment = (
+        <div className="text-center">Herhangi bir yorum bulunmamaktadır</div>
+    );
+
+    return <>{comments.length > 0 ? CommentsMapper : NoComment}</>;
 };
 
 export default MyComments;
