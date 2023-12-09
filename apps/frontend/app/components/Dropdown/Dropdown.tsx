53a88f28674588685476380ded610080694f7caa
Arif Trends <arfoz1245@gmail.com>
2023-12-09 21:29:46 +0100

    fix delete update

--- apps/frontend/app/components/Dropdown/Dropdown.tsx
+++ apps/frontend/app/components/Dropdown/Dropdown.tsx
@@ -11,7 +11,7 @@ export default function DropdownThreedots({ DeletePost }: { DeletePost: any }) {
         <Menu as="div" className="relative inline-block text-left">
             <div>
                 <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                    Options
+                    {/* Options */}
                     <ChevronDownIcon
                         className="-mr-1 h-5 w-5 text-gray-400"
                         aria-hidden="true"
@@ -28,7 +28,7 @@ export default function DropdownThreedots({ DeletePost }: { DeletePost: any }) {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
                         <Menu.Item>
                             {({ active }) => (
