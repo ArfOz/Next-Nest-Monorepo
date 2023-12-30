66a5b01049fa57024973d3dff8ffb7c437d22672
Arif Trends <arfoz1245@gmail.com>
2023-12-30 22:46:03 +0100

    Fix delete comment

--- apps/backend/src/comments/comments.service.ts
+++ apps/backend/src/comments/comments.service.ts
@@ -129,6 +129,16 @@ export class CommentsService {
             );
         }
 
+        const updateData: PrismaMongoDb.RestaurantsUpdateArgs = {
+            where: { id: permission.restaurantId },
+            data: {
+                stars: { decrement: permission.star },
+                comments: { decrement: 1 }
+            }
+        };
+
+        await this.restaurantDBService.update(updateData);
+
         const comment = await this.commentDBService.delete(where);
         return {
             Success: true,
