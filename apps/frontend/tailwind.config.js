80ffbce1c302ae01f0f9c197ebec6b60a6b0fbde
Arif Trends <arfoz1245@gmail.com>
2024-01-03 22:48:51 +0100

    add responsive design

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -7,14 +7,31 @@ module.exports = {
     mode: 'jit',
     purge: [
         join(__dirname, 'app/**/*.{js,ts,jsx,tsx}'),
-        ...createGlobPatternsForDependencies(__dirname),
+        ...createGlobPatternsForDependencies(__dirname)
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                '2xl': { max: '1535px' },
+                // => @media (max-width: 1535px) { ... }
+
+                xl: { max: '1279px' },
+                // => @media (max-width: 1279px) { ... }
+
+                lg: { max: '1023px' },
+                // => @media (max-width: 1023px) { ... }
+
+                md: { max: '767px' },
+                // => @media (max-width: 767px) { ... }
+
+                sm: { max: '639px' }
+                // => @media (max-width: 639px) { ... }
+            }
+        }
     },
     variants: {
-        extend: {},
+        extend: {}
     },
-    plugins: [],
+    plugins: []
 };
