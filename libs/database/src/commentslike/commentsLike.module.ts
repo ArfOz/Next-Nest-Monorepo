ea3709aa699fc21a336464fa734b9519b433d1de
Arif Trends <arfoz1245@gmail.com>
2024-01-08 22:27:57 +0100

    add commentlike database

--- libs/database/src/commentslike/commentsLike.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CommentsDBService } from './comments.Like.service';
-import { PrismaServicePGDB } from '../prisma';
-
-@Module({
-    providers: [CommentsDBService, PrismaServicePGDB]
-})
-export class CommentsModule {}
---
+++ libs/database/src/commentslike/commentsLike.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaServicePGDB } from '../prisma/index';
+import { Prisma } from '@prisma/postgres/client';
+
+@Injectable()
+export class CommentLikeDBService {
+    constructor(private prisma: PrismaServicePGDB) {}
+    async findUnique(where: Prisma.CommentLikeWhereUniqueInput) {
+        const data = await this.prisma.commentLike.findUnique({ where });
+        return data;
+    }
+
+    async findMany(where: Prisma.CommentLikeWhereInput) {
+        const data = await this.prisma.commentLike.findMany({
+            where
+        });
+
+        return data;
+    }
+
+    async addCommentsLike(data: Prisma.CommentLikeCreateInput) {
+        const response = await this.prisma.commentLike.create({ data });
+        return response;
+    }
+
+    async deleteCommentLike(where: Prisma.CommentLikeWhereUniqueInput) {
+        const response = await this.prisma.commentLike.delete({ where });
+        return response;
+    }
+    // async update({
+    //     where,
+    //     data
+    // }: {
+    //     where: Prisma.CommentWhereUniqueInput;
+    //     data: Prisma.CommentUpdateInput;
+    // }) {
+    //     const response = await this.prisma.comment.update({ where, data });
+
+    //     return response;
+    // }
+}
