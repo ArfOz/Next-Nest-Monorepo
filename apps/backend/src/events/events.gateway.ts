14a16e6e0105a3d8482265f9a728c87c353c44dd
Arif Trends <arfoz1245@gmail.com>
2024-01-13 21:52:21 +0100

    add .gitignore nx cache

--- apps/backend/src/events/events.gateway.ts
+++ apps/backend/src/events/events.gateway.ts
@@ -1,21 +1,29 @@
+import { Logger } from '@nestjs/common';
 import {
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(80, { namespace: 'events' })
 export class EventsGateway {
     @WebSocketServer()
     server: Server;
 
-    @SubscribeMessage('message')
-    handleMessage(client: any, payload: any): string {
-        return 'Hello world!';
+    private readonly logger = new Logger('Websocketgatewayyyy');
+
+    afterInit() {
+        this.logger.log('Initialized');
     }
 
-    sendMessage() {
-        this.server.emit('new message', 'heelo world from server');
+    @SubscribeMessage('message')
+    handleMessage(client: any, payload: any): string {
+        return 'Hello SERverrrrrrrr';
+    }
+
+    sendMessage(client: Socket, message) {
+        client.emit('selammmmmm');
+        // this.server.emit('newMessage', 'heelo world from server');
     }
 }
