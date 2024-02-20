64c1e7082523ae83d4bdbd9d40c7b2d2af549e83
Arif Trends <arfoz1245@gmail.com>
2024-02-20 23:31:29 +0100

    fix like button

--- apps/frontend/app/components/Comments/LikeButton.tsx
+++ apps/frontend/app/components/Comments/LikeButton.tsx
@@ -1,5 +1,19 @@
-import { HandThumbUpIcon } from '@heroicons/react/24/outline';
+import { HandThumbUpIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
 
 export const LikeButton = () => {
-	return <HandThumbUpIcon className="h-6 w-6 text-blue-500" />;
-};
+	const [liked, setLiked] = useState(false)
+	const handleClick = () => {
+		setLiked(!liked)
+	}
+	if (liked)
+		return (
+			<HandThumbUpIcon
+				className="h-6 w-6 text-red-600"
+				stroke="black"
+				fill="white"
+				strokeWidth={1.2}
+			/>
+		)
+	return <HandThumbUpIcon className="h-6 w-6" />
+}
--- libs/frontendlibs/src/lib/Calculator/Avg.calculator.ts
+++ libs/frontendlibs/src/lib/Calculator/Avg.calculator.ts
@@ -1,11 +1,11 @@
 // import { CommentDetails } from 'apps/frontend/app/Dtos/CityDetails.dto';
 export function AvgCalculator(restaurant: any): number {
-    // Average of total comment stars
-    let average;
-    if (restaurant.stars || restaurant.comments) {
-        const average = restaurant.stars / restaurant.comments;
-        return parseFloat(average.toFixed(1));
-    }
+	// Average of total comment stars
+	let average
+	if (restaurant.stars || restaurant.comments) {
+		const average = restaurant.stars / restaurant.comments
+		return parseFloat(average.toFixed(1))
+	}
 
-    return 0;
+	return 0
 }
